7b593988e721f8b862d6137793e23f6dd7376394
Andreas Entgelmeier <andreas@MBP-von-Andreas.fritz.box>
2024-03-15 10:25:27 +0100

    code from doc, fixed 2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 var http = require('http');
+const port = 8080;
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello World');
 });
-server.listen(8080, () => {
+server.listen(port, () => {
     console.log(`Server running at port ${port}/`);
   });
